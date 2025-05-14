d7a3c684afb6cecf5b300a469fb6dc2ee79620dc
wesley martins <wumartinss@gmail.com>
2025-05-13 21:42:05 -0300

    oq faltava

--- TRIO_NACOZINHA/TRIO_NACOZINHA/src/main/java/br/edu/ifsp/arq/controller/controllerUsuario/UsuarioServletDeletar.java
+++ TRIO_NACOZINHA/TRIO_NACOZINHA/src/main/java/br/edu/ifsp/arq/controller/controllerUsuario/UsuarioServletDeletar.java
@@ -7,35 +7,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UsuarioServletDeletar
- */
+import br.edu.ifsp.arq.dao.UsuarioDAO;
+
+
 @WebServlet("/UsuarioServletDeletar")
 public class UsuarioServletDeletar extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+       UsuarioDAO usuarioDao;
+   
     public UsuarioServletDeletar() {
         super();
-        // TODO Auto-generated constructor stub
+        usuarioDao = UsuarioDAO.getInstance_U();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ID recebido: " + request.getParameter("id"));
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String id = request.getParameter("id");
+		int id2 = Integer.parseInt(id);
+		usuarioDao.deletar(id2);
+		
+		request.setAttribute("receitas", usuarioDao.mostrarTodos());
+		getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
 	}
-
 }
